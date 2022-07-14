edfa49ab22aaabd30ef3d879c79f615802adb8d4
ceamattos <carlos@sensio.com.br>
2022-07-14 13:53:16 -0300

    Trying to learn some Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+const data = {name: "danilo", age: 20, local: "Juiz de Fora"};
+
 @Component({
   selector: 'vdb-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'PrimeiroAngular';
+  people = data;
 }
